46a85cf900409f61cff03d0ec4c5eba5b244b0af
pachkatrue <45824406+pachkatrue@users.noreply.github.com>
2026-09-22 20:41:14 +0600

    fix(store): remove hook exports from store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -31,7 +31,3 @@ export const store = configureStore({
 // Выводим типы из нашего хранилища
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-// Создаем типизированные хуки для использования в компонентах
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
